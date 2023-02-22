0dc9d3d9d4d8fd3d7120baa55d8f3525c3baf8b2
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-02-22 12:00:35 +0300

    feat: add like/trash

--- script/index.js
+++ script/index.js
@@ -1,6 +1,3 @@
-
-
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -32,46 +29,45 @@ const initialCards = [
 const CARDS_LIST = document.querySelector('.cards')
 const CARD = document.querySelector("#card")
 const TEMPLATE_CARD = document.querySelector("#template-card")
-
+const CARD_INFO = document.querySelector(".card__info");
 
 //попапы
 const popup = document.querySelector('.popup');
 const popupProfile = document.querySelector('.popup__profile');
 const popupCards = document.querySelector('.popup__cards');
 const popupImage = document.querySelector('.popup__image');
+
 //кнопки
 const profileButtonEdit = document.querySelector('.profile__button-edit');
-
 const popupButtonCloseProfile = document.querySelector('.popup__button-close-profile');
 const popupButtonCloseCards = document.querySelector('.popup__button-close-cards');
 const popupButtonCloseImage = document.querySelector('.popup__button-close-image');
 const cardsButtonAdd = document.querySelector('.profile__button-add');
-const cardButtonLike = document.querySelectorAll('.card__button-like');
-const cardButtonTrash = document.querySelectorAll('.card__button-trash');
 
+
+//форма User
 const profileName =  document.querySelector('.profile__name');
 const profileCareer = document.querySelector('.profile__career');
-//форма User
 const formProfile = document.querySelector('.popup__form-profile');
 const nameInput = document.querySelector('.popup__input_type_name');
 const careerInput = document.querySelector('.popup__input_type_career');
 
 // форма добавления карточек
 const formCards = document.querySelector('.popup__form-cards');
-const cardInput = document.querySelectorAll(".card__name");
-const urlInput = document.querySelectorAll(".card__img");
+const cardInput = document.querySelectorAll(".popup__input_card_name");
+const urlInput = document.querySelectorAll(".popup__input_card_img");
 //
 const cardImage = document.querySelector(".popup__image-large");
 const cardName = document.querySelector(".popup__image-title");
+
+
 //открытие попапа
 function openedPopup(popup) {
   popup.classList.add('popup_opened');
-  console.log('открытие');
 }
 //закрытие попапа
 function closedPopup(popup) {
   popup.classList.remove('popup_opened');
-  console.log('закрытие');
 }
 
 // попап блока User
@@ -89,22 +85,77 @@ function handleFormSubmit (evt) {
   closedPopup(popupProfile);
 }
 
+/*
+//добавление новой карточки
+ function createCard(item) {
+  const cardName = item.name;
+  const cardLink = item.link;
+  const cardElement = TEMPLATE_CARD.content.cloneNode(true);
+  cardElement.querySelector(".card__name").textContent = cardName;
+  const imageCard = cardElement.querySelector(".card__img");
+  imageCard.src = cardLink; 
+  
+  const cardButtonLike = cardElement.querySelectorAll('.card__button-like');
+  const cardButtonTrash = cardElement.querySelectorAll('.card__button-trash');
+
+  return cardElement;
+
+}
+
+const addNewCard = (item) => {
+  CARDS_LIST.prepend(createCard(item));
+};
+
+initialCards.forEach(function (item) {
+  CARDS_LIST.append(createCard(item));
+});
+*/
+
 //добавление 6 карточек из массива initialCards на страничку
 
+function addCard(name, link) {
+  const card = TEMPLATE_CARD.content.querySelector('.card');
+  const imgElement = card.querySelector('.card__img');
+  imgElement.setAttribute('src', link);
 
-//удаление карточек
-cardButtonTrash.forEach((el) => {
-  el.addEventListener("click", () => {
-    el.closest(".card").remove(); // не поняла как работает метод closest
-  });
-});
+  const titleElement = card.querySelector('.card__name');
+  titleElement.textContent = name;
+  imgElement.alt = name;
 
-//лайки всех карточек
-cardButtonLike.forEach(function (likeActive) {
-  likeActive.addEventListener("click", () => {
-    likeActive.classList.toggle("card__button-like_active");
-  });
-});
+  const cloneCard = card.cloneNode(true);
+
+  CARDS_LIST.append(cloneCard);
+
+  //лайки и корзина
+  const likeButton = cloneCard.querySelector(".card__button-like");
+  const trashButton = cloneCard.querySelector(".card__button-trash")
+
+  function likeCard() {
+    likeButton.classList.toggle("card__button-like_active");
+  };
+
+  function deleteCard() {
+    cloneCard.remove();
+  };
+  //попап с большой картинкой
+  function openedCardLarge() {
+    console.log('click');
+  }
+
+  //слушатели на карточке 
+  likeButton.addEventListener("click", likeCard);
+  trashButton.addEventListener("click", deleteCard);
+  imgElement.addEventListener("click", openedCardLarge);// большая картинка
+  return cloneCard;
+}
+
+
+function addAllCard() {
+  initialCards.forEach((card)=>addCard(card.name,card.link))
+}
+
+addAllCard();
+
 
 //открытие попапа User
 profileButtonEdit.addEventListener('click', handleProfilePopup);
@@ -114,17 +165,20 @@ cardsButtonAdd.addEventListener("click", function () {
   openedPopup(popupCards);
 });
 
-//открытие попапа с большой картинкой
-urlInput.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    openedPopup(popupImage);
-  });
-});
-
 //отправка формы User
 formProfile.addEventListener('submit', handleFormSubmit);
 
 //отправка формы добавления карточек
+formCards.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const placeElement = addCard(cardInput, urlInput);
+  CARDS_LIST.prepend(placeElement);
+  closedPopup(popupCards);
+  console.log(cardInput.value);
+  console.log(urlInput.value);
+  evt.target.reset();
+});
+
 
 
 // все кнопки X закрытия попапа
@@ -137,25 +191,3 @@ closedPopup(popupCards);
 popupButtonCloseImage.addEventListener('click', function() {
   closedPopup(popupImage);
 });
-
-function addAllCard(){
-  initialCards.forEach((card)=>addCard(card.name,card.link))
-}
-
-addAllCard()
-
-function addCard(name,link){
-  const card = TEMPLATE_CARD.content.querySelector('.card')
-
-  const imgElement = card.querySelector('.card__img')
-  imgElement.setAttribute('src',link)
-
-  const titleElement = card.querySelector('.card__name')
-  titleElement.textContent = name
-
-  const cloneCard = card.cloneNode(true)
-
-
-  CARDS_LIST.prepend(cloneCard)
-}
-
